f6c24bdd43c32a79d1a7d27a70c433f30059718d
Connor Pak <connorpak@Connors-MacBook-Pro.local>
2023-09-26 12:15:47 -0700

    penis LOL

--- src/main/java/u3pp/LeapYear.java
+++ src/main/java/u3pp/LeapYear.java
@@ -12,6 +12,13 @@ public class LeapYear {
 	 * YOUR COMMENTS HERE
 	 */
 	public static boolean isLeapYear(int year) {
+		if( (year%100==0) && (year%400!=0)){
+			return false;
+		}
+		if ((year>=1582) && (year%4==0)){
+			return true;
+		}
+		return false;
 		/* YOUR CODE HERE */
 	}
 }
--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -11,17 +11,32 @@ import java.util.Scanner;
 class Main {
 
 	public static void main(String[] args) {
-
+		
 		//YOUR CODE HERE
+		Scanner sc=new Scanner(System.in);
 
-		System.out.print("Enter a year (or enter STOP to end): ");
+		System.out.println("Enter a year (or enter STOP to end): ");
 		String input = sc.nextLine();
+		if(input=="STOP") {
+			sc.close();
+		}
+		
+
 
 		while (!input.toUpperCase().equals("STOP")) {
-			/* YOUR CODE HERE */
-			System.out.print("Enter a year (or enter STOP to end): ");
-			//YOUR CODE HERE
+			int penis=Integer.parseInt(input);
+			
+			if(LeapYear.isLeapYear(penis)==true) {
+				System.out.println(penis + " is a leap year");
+			}
+			else {
+				System.out.println(penis + " is not a leap year");
+			}
+
+			System.out.println("Enter a year (or enter STOP to end): ");
+			input=sc.nextLine();
 		}
+		sc.close();
 		//YOUR CODE HERE
 	}
 }
